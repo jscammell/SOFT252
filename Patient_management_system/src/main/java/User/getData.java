ff6bf050d5973359fe6ba21e7b9d09b4f87e461c
jscammell <44771964+jscammell@users.noreply.github.com>
2019-12-02 12:41:58 +0000

    Retrieved data from the text file in the class getdata

--- Patient_management_system/src/main/java/User/getData.java
+++ Patient_management_system/src/main/java/User/getData.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package User;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+
 
 /**
  *
@@ -11,4 +16,57 @@ package User;
  */
 public class getData {
     
+    private ArrayList<Users>patients;
+    private ArrayList <Users>administrators;
+    private ArrayList<Users>secretary;
+    private ArrayList<Users>doctors;
+    
+    public void readPatients(ArrayList<Patient>patients)throws Exception
+    {
+        File file = new File("C:\\Users\\test.txt");
+        
+        BufferedReader br = new BufferedReader(new FileReader(file));
+          
+            String userId;
+            String password;
+            String first_Name;
+            String last_Name;
+            String phone_Number;
+            String address;
+            String postcode;
+            String date_Of_Birth;
+            int age;
+            String sex;
+            String nhs_Number;
+        
+        while ((br.readLine())!=null) {
+            userId = br.readLine();
+            password = br.readLine();
+            first_Name = br.readLine();
+            last_Name = br.readLine();
+            phone_Number = br.readLine();
+            address = br.readLine();
+            postcode = br.readLine();
+            date_Of_Birth = br.readLine();
+            age = Integer.parseInt(br.readLine());
+            sex = br.readLine();
+            nhs_Number = br.readLine();
+            
+            Users newpatient = new Patient(userId, password, first_Name, last_Name, phone_Number, address, postcode, date_Of_Birth, age, sex, nhs_Number); 
+            
+            addpatient(newpatient);
+            
+            patients = new ArrayList<Patient>();
+        } 
+    }
+    
+    public void addpatient(Users patient)
+        {
+        patients.add(patient);
+        }
+       
+        public void removepatient(Users patient)
+        {
+        patients.remove(patient);
+        }
 }
